3345057b3b691c2b5337ff1ba2faa76ae29e74c0
Grant Fox <granttfox@gmail.com>
2019-06-03 11:08:16 -0600

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,11 @@ export default function Header(props) {
             {props.appliedBackground.user &&
             <h1>{props.appliedBackground.user}'s Background</h1>
             }
+            {props.appliedBackground.user &&
+            <h3>
+            {`Your background is "linear-gradient(to right, ${props.appliedBackground.color1}, ${props.appliedBackground.color2})`}
+            </h3>
+        }
             
         </header>
     )
